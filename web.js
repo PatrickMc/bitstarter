7e3c6d10c295ac5e53d3c413a15760134b8196e3
root <root@phenom-virtual-machine.(none)>
2013-07-11 12:54:57 +1000

    Working step 1

--- web.js
+++ web.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	fs.readFile('index.html', 'utf8', function(err,data) {
+	console.info('Got request');
+	fs.readFile('./index.html', 'utf-8', function(err,data) {
+		if (err) {
+			throw err;
+		}
 		response.send(data);
 	});
+//	response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
